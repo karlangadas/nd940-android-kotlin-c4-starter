b6e47c80408a25114979a5fc2776c22d14262666
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2023-11-04 15:20:44 +0000

    [LocationReminder] Add final view model test

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModel.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModel.kt
@@ -1,6 +1,7 @@
 package com.udacity.project4.locationreminders.savereminder
 
 import android.app.Application
+import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.maps.model.PointOfInterest
@@ -67,7 +68,8 @@ class SaveReminderViewModel(val app: Application, val dataSource: ReminderDataSo
     /**
      * Validate the entered data and show error to the user if there's any invalid data
      */
-    private fun validateEnteredData(reminderData: ReminderDataItem): Boolean {
+    @VisibleForTesting
+    fun validateEnteredData(reminderData: ReminderDataItem): Boolean {
         if (reminderData.title.isNullOrEmpty()) {
             showSnackBarInt.value = R.string.err_enter_title
             return false
--- starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,70 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
+import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    private lateinit var viewModel: SaveReminderViewModel
+    private lateinit var dataSource: FakeDataSource
+    private val reminders = mutableListOf<ReminderDTO>()
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
 
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
+    @Before
+    fun setUp() {
+        stopKoin()
+        // Given a fresh ViewModel
+        dataSource = FakeDataSource(reminders)
+        viewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+    }
+
+    @Test
+    fun validateEnteredData_validData() {
+        // WHEN reminder is valid
+        val reminder = ReminderDataItem(
+            "Reminder",
+            "Description",
+            "Location 1",
+            25.0,
+            50.0
+        )
+        // THEN reminder is validated
+        assertThat(viewModel.validateEnteredData(reminder)).isEqualTo(true)
+    }
+
+    @Test
+    fun validateEnteredData_invalidData() {
+        // WHEN reminder is valid
+        val reminder = ReminderDataItem(
+            "",
+            "Description",
+            "Location 1",
+            25.0,
+            50.0
+        )
+        // THEN reminder is invalidated
+        assertThat(viewModel.validateEnteredData(reminder)).isEqualTo(false)
+    }
 }
