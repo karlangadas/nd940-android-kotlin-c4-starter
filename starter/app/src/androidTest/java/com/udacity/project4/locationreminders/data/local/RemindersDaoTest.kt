e72558f63082672d63e1d0a7455a270906fbc711
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2023-11-04 11:32:16 +0000

    [LocationReminder] Add db test

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -12,10 +12,9 @@ import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi;
-import kotlinx.coroutines.test.runBlockingTest
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.notNullValue
-import org.hamcrest.MatcherAssert.assertThat
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import com.google.common.truth.Truth.assertThat
 import org.junit.After
 import org.junit.Test
 
@@ -25,6 +24,56 @@ import org.junit.Test
 @SmallTest
 class RemindersDaoTest {
 
-//    TODO: Add testing implementation to the RemindersDao.kt
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var database: RemindersDatabase
+
+    @Before
+    fun initDB(){
+        database = Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(),
+        RemindersDatabase::class.java).build()
+    }
+
+    @After
+    fun closeDB(){
+        database.close()
+    }
+
+    @Test
+    fun insertReminderAndGetById() = runTest {
+        // GIVEN - insert reminder
+        val reminder =  ReminderDTO("Reminder 1", "description 1", "location 1", 15.0, 15.0)
+        database.reminderDao().saveReminder(reminder)
+        advanceUntilIdle()
+
+        // WHEN - Get the reminder by id from the database
+        val loaded = database.reminderDao().getReminderById(reminder.id)
+
+        // THEN - The loaded data contains the expected values
+        assertThat(loaded).isNotNull()
+        assertThat(loaded?.id).isEqualTo(reminder.id)
+        assertThat(loaded?.title).isEqualTo(reminder.title)
+        assertThat(loaded?.description).isEqualTo(reminder.description)
+        assertThat(loaded?.location).isEqualTo(reminder.location)
+        assertThat(loaded?.latitude).isEqualTo(reminder.latitude)
+        assertThat(loaded?.longitude).isEqualTo(reminder.longitude)
+    }
+
+    @Test
+    fun emptyDatabaseAndGetById() = runTest {
+        // GIVEN - empty db
+        database.reminderDao().deleteAllReminders()
+        advanceUntilIdle()
+
+        // WHEN - Get the reminder by id from the database
+        val loaded = database.reminderDao().getReminderById("")
+
+        // THEN - The loaded data is null
+        assertThat(loaded).isNull()
+    }
 }
