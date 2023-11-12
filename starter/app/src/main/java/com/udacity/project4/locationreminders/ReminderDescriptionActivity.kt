dab23b1fff6c900d59d16d3a25db5bdc3a073ffa
Karla Pedraza <karla.pedraza@pucp.edu.pe>
2023-11-12 12:29:12 +0000

    [LocationReminder] implement ReminderDescriptionActivity

--- starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -3,21 +3,37 @@ package com.udacity.project4.locationreminders
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityReminderDescriptionBinding
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
+import com.udacity.project4.locationreminders.savereminder.selectreminderlocation.SelectLocationFragment
 
 /**
  * Activity that displays the reminder details after the user clicks on the notification
  */
-class ReminderDescriptionActivity : AppCompatActivity() {
+class ReminderDescriptionActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityReminderDescriptionBinding
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+
+    private lateinit var reminder: ReminderDataItem
 
     companion object {
+        private val TAG = ReminderDescriptionActivity::class.java.simpleName
         private const val EXTRA_ReminderDataItem = "EXTRA_ReminderDataItem"
+        private const val DEFAULT_ZOOM = 15f
 
         // Receive the reminder object after the user clicks on the notification
         fun newIntent(context: Context, reminderDataItem: ReminderDataItem): Intent {
@@ -31,6 +47,42 @@ class ReminderDescriptionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val layoutId = R.layout.activity_reminder_description
         binding = DataBindingUtil.setContentView(this, layoutId)
-        // TODO: Add the implementation of the reminder details
+
+        reminder = intent.extras?.getParcelable(EXTRA_ReminderDataItem)?: return
+        binding.reminderDataItem = reminder
+
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        setMapLocation(googleMap)
+    }
+
+    private fun setMapLocation(googleMap: GoogleMap) {
+        Log.v(TAG, "reminder: $reminder")
+        val lat = reminder.latitude?: return
+        val long = reminder.longitude?: return
+        val latLng = LatLng(lat, long)
+        googleMap.addMarker(MarkerOptions().position(latLng).title(reminder.location))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
 }
